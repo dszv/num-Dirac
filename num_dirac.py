0530e90d1f7449b9bc12ba778499d58c26bab8d4
dszv <diego.suarez@uni.pe>
2020-02-20 17:55:39 -0500

    Changes in notation

--- num_dirac.py
+++ num_dirac.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Dec 26 23:04:38 2017
 
-@author: Imhotep
+@author: Diego S
 
 Numerov method for Dirac equation: 3d step potential
 """
@@ -12,70 +12,61 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 from scipy.optimize import brentq
 
-l = float(input("Angular momentum l:"))
-L = float(input("Width of the potential:"))
-Vo = float(input("Value of the potential:"))
-N = int(input("Number of steps (~10000):"))
-h = float(3*L/N)
-psi = np.zeros(N,dtype="float64") #wave function
-psi[0] = 0
-psi[1] = h
+l, L, Vo = 0.0, 3.0, 1.5 #angular momentum, potential widht, potential value
+x_max = 3*L #max value of x where psi tends to zero
+h = 0.0001 #step value for solve the ode
+e = 0.1 #step value for energies of the eigenstates
+N = int(x_max/h) #number of iterations
+psi = np.zeros(N,dtype="double") #wave function
+psi[0], psi[1] = 0, h #initial conditions
+
 
 def V(x,E):
-    """
-    Effective potential function.
-    """
+    #Effective potential function
     if x > L:
         return -E**2+1+l*(l+1)/x**2
     else:
         return -(E+Vo)**2+1+l*(l+1)/x**2
     
-def Wavefunction(energy):
-    """
-    Calculates wave function psi for the given value
-    of energy E and returns value at point xmax
-    """
-    global psi
-    global E
-    E=energy
+def Wavef(E):
+    # Calculates psi(x_max)
     for i in range(2,N):
         psi[i]=(2*(1+5*(h**2)*V(i*h,E)/12)*psi[i-1]-(1-(h**2)*V((i-1)*h,E)/12)*psi[i-2])/(1-(h**2)*V((i+1)*h,E)/12)       
     return psi[-1]
 
-def find_energy_levels(x,y):
-    """
-    Gives all zeroes in y = psi_max, x=en
-    """
+def find_E_levels(energies,psi_max):
+    # Find all zeroes of psi(3L) = 0
     zeroes = []
-    s = np.sign(y)
-    for i in range(len(y)-1):
+    s = np.sign(psi_max)
+    for i in range(len(psi_max)-1):
         if s[i]+s[i+1] == 0: #sign change
-            zero = brentq(Wavefunction, x[i], x[i+1])
+            zero = brentq(Wavef, energies[i], energies[i+1])
             zeroes.append(zero)
     return zeroes
 
-def main():
-    
-    energies = np.linspace(-1,1,int(10*Vo))   # vector of energies where we look for the stable states
-    psi_max = []  # vector of wave function at x = 3L for all of the energies in energies
-    for energy in energies:
-        psi_max.append(Wavefunction(energy))     # for each energy find the the psi_max at xmax
-    E_levels = find_energy_levels(energies,psi_max)   # now find the energies where psi_max = 0  
-    print ("Energies for the bound states are: ")
-    for E in E_levels:
-        print ("%.2f" %E)
-    # Plot the wavefunctions for first 4 eigenstates
-    x = np.linspace(0, 3*L, N)
-    plt.figure()
-    for E in E_levels:
-        Wavefunction(E)
-        plt.plot(x, psi, label="E = %.2f"%E)
-    plt.legend(loc="upper right")
-    plt.xlabel('$r$')
-    plt.ylabel('$u(r)$')
-    plt.savefig('num_dirac.pdf', bbox_inches='tight')
-    
-if __name__ == "__main__":
-    main()
 
+energies = np.linspace(-1, 1, int(2/e)) #vector of energies where we look for the stable states
+psi_max = []  # vector for values of the wave function at x = 3L
 
+for E in energies:
+    psi_max.append(Wavef(E)) #for each energy find the psi_max at xmax
+
+E_levels = find_E_levels(energies, psi_max) #now find the energies where psi_max = 0
+
+print ("Energies for the bound states are: ")
+for E in E_levels:
+    print ("%.3f" %E)
+
+# Plot the wavefunctions for the eigenstates
+x = np.linspace(0, x_max, N)
+plt.figure()
+
+for E in E_levels:
+    Wavef(E)
+    plt.plot(x, psi, label="E = %.3f"%E)
+
+plt.legend(loc="upper right")
+plt.xlabel('$r$')
+plt.ylabel('$u(r)$')
+plt.savefig('num_dirac.pdf', bbox_inches='tight')    
+plt.show()
