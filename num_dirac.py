14a5d95384570646cac1dc2613c2e5ef4d64540d
dszv <diego.suarez@uni.pe>
2020-02-21 01:05:22 -0500

    Changed structure

--- num_dirac.py
+++ num_dirac.py
@@ -13,7 +13,7 @@ plt.style.use('ggplot')
 from scipy.optimize import brentq
 from scipy import integrate
 
-l, L, Vo = 0.0, 10.0, 1.5 #angular momentum, potential widht, potential value
+l, L, Vo = 0.0, 10.0, 1.5 #angular momentum, potential width, potential value
 h = 0.001 #step value for solve the ode
 e = 0.1 #step value for energies of the eigenstates
 N = int(3*L/h) #number of iterations
@@ -31,8 +31,8 @@ def V(x,E):
     
 def Wavef(E):
     #Calculates the psi(3L) = 0
-    for i in range(2,N):
-        psi[i]=(2*(1+5*(h**2)*V(i*h,E)/12)*psi[i-1]-(1-(h**2)*V((i-1)*h,E)/12)*psi[i-2])/(1-(h**2)*V((i+1)*h,E)/12)       
+    for i in range(2, N):
+        psi[i] = (2 * (1 + 5 * (h**2) * V(i*h, E)/12) * psi[i-1] - (1 - (h**2) * V((i-1)*h, E)/12) * psi[i-2])/(1 - (h**2) * V((i+1) * h, E)/12)
     return psi[-1]
 
 def find_E_levels(energies,psi_max):
@@ -46,30 +46,34 @@ def find_E_levels(energies,psi_max):
     return zeroes
 
 
-energies = np.linspace(-1, 1, int(2/e)) #vector of energies where we look for the stable states
-psi_max = []  # vector for values of the wave function at x = 3L
+def main():
+    energies = np.linspace(-1, 1, int(2/e)) #vector of energies where we look for the stable states
+    psi_max = []  # vector for values of the wave function at x = 3L
+    
+    for E in energies:
+        psi_max.append(Wavef(E)) #for each energy find the psi_max at xmax
+    
+    E_levels = find_E_levels(energies, psi_max) #now find the energies where psi_max = 0
+    
+    # Plot the wavefunctions for the eigenstates
+    x = np.linspace(0, 3*L, N)
+    plt.figure()
+    print ("Energies for the bound states are: ")
+    
+    for E in E_levels:
+        print ("E =", "%.3f"%E)
+        Wavef(E)
+        Norm = np.sqrt(integrate.simps(psi**2, x)) #finding normalization factor
+        psi_norm = psi/Norm
+        print("Psi_norm(max):", psi_norm[-1])
+        print ("Norm ->", integrate.simps(psi_norm**2, x))
+        plt.plot(x, psi_norm, label="%.3f"%E)
+    
+    plt.legend(loc="upper right")
+    plt.xlabel('$r$')
+    plt.ylabel('$u(r)$')
+    plt.tight_layout()
+    plt.savefig('wavefunction.pdf')
+    plt.show()
 
-for E in energies:
-    psi_max.append(Wavef(E)) #for each energy find the psi_max at xmax
-
-E_levels = find_E_levels(energies, psi_max) #now find the energies where psi_max = 0
-
-# Plot the wavefunctions for the eigenstates
-x = np.linspace(0, 3*L, N)
-plt.figure()
-print ("Energies for the bound states are: ")
-
-for E in E_levels:
-    print ("E =", "%.3f"%E)
-    Wavef(E)
-    Norm = np.sqrt(integrate.simps(psi**2, x)) #finding normalization factor
-    psi_norm = psi/Norm
-    print("Psi_norm(max):", psi_norm[-1])
-    print ("Norm ->", integrate.simps(psi_norm**2, x))
-    plt.plot(x, psi_norm, label="%.3f"%E)
-
-plt.legend(loc="upper right")
-plt.xlabel('$r$')
-plt.ylabel('$u(r)$')
-plt.savefig('num_dirac.pdf', bbox_inches='tight')    
-plt.show()
+main()
